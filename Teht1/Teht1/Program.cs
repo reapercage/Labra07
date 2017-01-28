a8ab6ae5929327f5e609f4be009d2ad6d0b6b811
reapercage <h4026@student.jamk.fi>
2017-01-28 21:26:07 +0200

    Toimii

---
+++ Teht1/Teht1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool jatketaan = true;
+            List<string> lines = new List<string>();
+            string line;
+            while(jatketaan)
+            {
+                Console.Write("Gimme your line (exg to exit) > ");
+                line = Console.ReadLine();
+                if(line == "exg")
+                {
+                    jatketaan = false;
+                }
+                else
+                {
+                    lines.Add(line);
+                }
+            }
+            //tiedostoon kirjoittaminen
+            System.IO.StreamWriter tiedosto = new System.IO.StreamWriter("lines.txt");
+            foreach(string laini in lines)
+            {
+                tiedosto.WriteLine(laini);
+            }
+            tiedosto.Close();
+            //tiedoston lukeminen
+            string text = System.IO.File.ReadAllText("lines.txt");
+            System.Console.WriteLine("\nlines.txt:\n" + text);
+            Console.ReadKey();
+        }
+    }
+}
