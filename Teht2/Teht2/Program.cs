7219a7db1a933632da941db89be27556b40a90a3
reapercage <h4026@student.jamk.fi>
2017-01-28 22:24:26 +0200

    Teht2 alku

---
+++ Teht2/Teht2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Exist
+            //string curFile = @"c:\testi.txt";
+            //Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\testi.txt");
+            System.Console.WriteLine("Contents of testi.txt:\n" + lines[0]);
+            Console.ReadKey();
+        }
+    }
+}
