005c6533cd445d2daab0904286125ea1221184cf
reapercage <h4026@student.jamk.fi>
2017-01-29 11:38:51 +0200

    Teht2 alkua

--- Teht2/Teht2/Program.cs
+++ Teht2/Teht2/Program.cs
@@ -14,7 +14,23 @@ namespace Teht2
             //string curFile = @"c:\testi.txt";
             //Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
             string[] lines = System.IO.File.ReadAllLines(@"C:\testi.txt");
-            System.Console.WriteLine("Contents of testi.txt:\n" + lines[0]);
+            int numberOfLines = lines.Length;
+            //tilalle dynaaminen list
+            List<string> nimet = new List<string>();
+            string check = lines[0];
+            nimet.Add(check);
+            int namecount = 0;
+            for(int i = 0; i < numberOfLines; i++)
+            {
+                if(check == lines[i])
+                {
+                    namecount++;
+                }
+            }
+            Console.WriteLine("Tiedosto testi.txt sisältää " + numberOfLines + " riviä ja " + namecount + " " + check + " nimeä.");
+            Console.WriteLine("testi.txt:\n" + lines[0]);
+            Console.WriteLine("\n" + nimet[0]);
+            //if string[0]
             Console.ReadKey();
         }
     }
