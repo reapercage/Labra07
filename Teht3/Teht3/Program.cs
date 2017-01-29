d60ba7c25fdca5f6e2a00327614f3e8251ca3599
reapercage <h4026@student.jamk.fi>
2017-01-29 15:53:21 +0200

    Teht3 toimii jotenkin mutta huonosti

--- Teht3/Teht3/Program.cs
+++ Teht3/Teht3/Program.cs
@@ -10,32 +10,50 @@ namespace Teht3
     {
         static void Main(string[] args)
         {
-            bool jatka = true;
+            //bool jatka = true;
             string luku;
             int kokonaisluku;
             double reaali;
             bool onLuku = true;
             bool onReaali = false;
+            bool tyhjä = false;
 
-            while(jatka)
+            List<int> kokonaisluvut = new List<int>();
+            List<double> reaaliluvut = new List<double>();
+
+            while(onLuku == true)
             {
                 Console.Write("Anna luku > ");
                 luku = Console.ReadLine();
-                //if luku contains dot => double else if luku on numero => int else end
-                foreach (char c in luku)
+                //. ei toimi if . muuta se ,
+
+                if (luku == "exit")
                 {
-                    if (c < '0' || c > '9')
-                    {
-                        onLuku = true;
-                        //return false;
-                    }
-                    else
-                    {
-                        onLuku = false;
-                    }
+                    onLuku = false;
+                    break;
                 }
-                if (onLuku == true)
+                else
                 {
+                    foreach (char c in luku)
+                    {
+                        if (c == null || c == '.')
+                        {
+                            onLuku = false;
+                        }
+                    }
+                    foreach (char c in luku)
+                    {
+                        if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '.' || c == ',')
+                        {
+                            onLuku = true;
+                            //return false;
+                        }
+                        else
+                        {
+                            onLuku = false;
+                            break;
+                        }
+                    }
                     foreach (char c in luku)
                     {
                         if (c == '.' || c == ',')
@@ -43,21 +61,32 @@ namespace Teht3
                             onReaali = true;
                         }
                     }
+                    if (onReaali == true)
+                    {
+                        reaali = System.Convert.ToDouble(luku);
+                        reaaliluvut.Add(reaali);
+                    }
+                    else if (onLuku == true && tyhjä != false)
+                    {
+                        kokonaisluku = System.Convert.ToInt32(luku);
+                        kokonaisluvut.Add(kokonaisluku);
+                    }
                 }
-                if (onReaali == true)
-                {
-                    reaali = System.Convert.ToDouble(luku);
-                }
-                else if (onLuku == true)
-                {
-                    kokonaisluku = System.Convert.ToInt32(luku);
-                }
-                else
-                {
-                    //lopeta
-                    jatka = false;
-                }
+                
             }
+            //tiedostoon kirjoittaminen
+            System.IO.StreamWriter tiedosto = new System.IO.StreamWriter("kokonaisluvut.txt");
+            foreach (int lupu in kokonaisluvut)
+            {
+                tiedosto.WriteLine(lupu);
+            }
+            tiedosto.Close();
+            System.IO.StreamWriter tiedosto2 = new System.IO.StreamWriter("reaaliluvut.txt");
+            foreach (int tupu in reaaliluvut)
+            {
+                tiedosto2.WriteLine(tupu);
+            }
+            tiedosto2.Close();
             Console.WriteLine("Lopetetaan...");
             Console.ReadKey();
         }
